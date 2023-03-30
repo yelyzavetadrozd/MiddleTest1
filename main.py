62a3a9e1eb77242e4fca69d7883eb82ca878d082
Yelyzaveta Drozd <elizavetadrozd266@knu.ua>
2023-03-30 11:31:39 +0300

    create new function sort_by_population

--- main.py
+++ main.py
@@ -9,13 +9,23 @@ def sort_by_area(file_name):
     items = {}
     with open(file_name) as file:
         for line in file:
-            country, area, population = parse_line(line)
+            country, area, _ = parse_line(line)
             items[country] = area
     sorted_items_by_area = sorted(items.items(), key=lambda x: x[1])
     return sorted_items_by_area
 
+def sort_by_population(file_name):
+    items = {}
+    with open(file_name) as file:
+        for line in file:
+            country, _, population = parse_line(line)
+            items[country] = int(population)
+    sorted_items_by_population = sorted(items.items(), key=lambda x: x[1])
+    return sorted_items_by_population
+
 def main(file_name):
     print (sort_by_area(file_name))
+    print(sort_by_population(file_name))
 
 
 if __name__ == '__main__':
