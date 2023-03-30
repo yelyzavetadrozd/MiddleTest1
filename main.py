c2b424a81e37cf1865cbab1aed4d0d3bf2803fb1
Yelyzaveta Drozd <elizavetadrozd266@knu.ua>
2023-03-30 11:13:01 +0300

    create new function parse_line

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+def parse_line(line: str) -> tuple:
+    items = line.split()
+    if len(items) < 3:
+        raise ValueError(f'Not enough values to unpack (expected at least 4, got {len(items)})')
+    country, area, population = items
+    return country, area, population
+
 def main(file_name):
     pass
 
